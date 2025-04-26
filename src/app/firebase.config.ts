ae2beb8c67cbf3dd96521ff1434fd6d61aac2405
Francisco J <franlsym@users.noreply.github.com>
2025-04-26 22:11:16 +0100

    update Firebase configuration with project-specific settings

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -3,12 +3,11 @@
  * Replace the following values with your Firebase project settings.
  */
 export const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: 'YOUR_APP_ID',
-  // Optional
-  measurementId: 'YOUR_MEASUREMENT_ID'
+  apiKey: "AIzaSyCmJbl6PG997cReFEjkeqh21zAnIvt8fMA",
+  authDomain: "learningangular-e7b02.firebaseapp.com",
+  projectId: "learningangular-e7b02",
+  storageBucket: "learningangular-e7b02.firebasestorage.app",
+  messagingSenderId: "127163216719",
+  appId: "1:127163216719:web:bc3d5a9b0289fb347ecb3a",
+  measurementId: "G-7W8KMXTFXL"
 };
